fd359c33cbddc07fcf0b3895fc5b7448e7f75d4b
Vadim Kaushan <admin@disasm.info>
2019-12-25 23:19:51 +0300

    [stm32-log] Add support for the NUCLEO-F446ZE board

--- stm32-log/src/lib.rs
+++ stm32-log/src/lib.rs
@@ -2,13 +2,14 @@
 
 #[cfg(not(feature = "target-selected"))]
 compile_error!("This crate requires one of the following device features enabled:
-        nucleo-f429zi");
+        nucleo-f429zi
+        nucleo-f446ze");
 
-#[cfg(feature = "nucleo-f429zi")]
+#[cfg(any(feature = "nucleo-f429zi", feature = "nucleo-f446ze"))]
 mod nucleo_f429zi;
-#[cfg(feature = "nucleo-f429zi")]
+#[cfg(any(feature = "nucleo-f429zi", feature = "nucleo-f446ze"))]
 pub use nucleo_f429zi::configure;
-#[cfg(feature = "nucleo-f429zi")]
+#[cfg(any(feature = "nucleo-f429zi", feature = "nucleo-f446ze"))]
 pub(crate) use nucleo_f429zi::write_bytes;
 
 #[cfg(feature = "target-selected")]
