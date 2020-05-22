82a8726f1650c102a7b9c373f827f0154c075191
Vadim Kaushan <admin@disasm.info>
2020-05-22 12:24:47 +0300

    Add HS support to the test_class examples for F429 and F446

--- example-f429zi-board/examples/test_class.rs
+++ example-f429zi-board/examples/test_class.rs
@@ -5,7 +5,10 @@ extern crate panic_semihosting;
 
 use cortex_m_rt::entry;
 use stm32f4xx_hal::{prelude::*, stm32};
+#[cfg(feature = "fs")]
 use stm32f4xx_hal::otg_fs::{USB, UsbBus};
+#[cfg(feature = "hs")]
+use stm32f4xx_hal::otg_hs::{USB, UsbBus};
 use usb_device::test_class::TestClass;
 
 static mut EP_MEMORY: [u32; 1024] = [0; 1024];
@@ -24,14 +27,26 @@ fn main() -> ! {
         .require_pll48clk()
         .freeze();
 
+    #[cfg(feature = "fs")]
     let gpioa = dp.GPIOA.split();
+    #[cfg(feature = "hs")]
+    let gpiob = dp.GPIOB.split();
 
+    #[cfg(feature = "fs")]
     let usb = USB {
         usb_global: dp.OTG_FS_GLOBAL,
         usb_device: dp.OTG_FS_DEVICE,
         usb_pwrclk: dp.OTG_FS_PWRCLK,
         pin_dm: gpioa.pa11.into_alternate_af10(),
         pin_dp: gpioa.pa12.into_alternate_af10(),
+    };
+    #[cfg(feature = "hs")]
+    let usb = USB {
+        usb_global: dp.OTG_HS_GLOBAL,
+        usb_device: dp.OTG_HS_DEVICE,
+        usb_pwrclk: dp.OTG_HS_PWRCLK,
+        pin_dm: gpiob.pb14.into_alternate_af12(),
+        pin_dp: gpiob.pb15.into_alternate_af12(),
     };
 
     let usb_bus = UsbBus::new(usb, unsafe { &mut EP_MEMORY });
